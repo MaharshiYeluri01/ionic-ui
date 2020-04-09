057a6e17775f94f15c992c1fd6c67e5b26c9b5a2
MaharshiYeluri01 <yeluririshi@gmail.com>
2020-04-09 17:13:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Camera } from '@ionic-native/camera/ngx';
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
+    Camera,
+    Geolocation,
+    NativeGeocoder,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
-
+import { Component,NgZone } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  clickedImage: string;
+  latitude: any = 0; //latitude
+  longitude: any = 0; //longitude
 
+  cam_options: CameraOptions = {
+    quality: 30,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+  }
+  options = {
+    timeout: 10000, 
+    enableHighAccuracy: true, 
+    maximumAge: 3600
+  };
+  constructor(private camera: Camera, private geolocation: Geolocation,private httpClient:HttpClient) { }
+
+
+
+  getCurrentCoordinates() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  captureImage() {
+    this.getCurrentCoordinates()
+    this.camera.getPicture(this.cam_options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.clickedImage = base64Image;
+      console.log(this.clickedImage)
+
+    }, (err) => {
+      console.log(err);
+      // Handle error
+    });
+  }
+
+upload(){
+  let data=[this.clickedImage,this.longitude,this.latitude]
+  console.log(data)
+  this.httpClient.post('http://127.0.0.1:5000/ ', data)
+      .subscribe(res => {
+        console.log(res);
+        alert('SUCCESS !!');
+      })
+}
 }
